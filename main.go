4c08e6e5825fc0f81a109a7d3ae06679c8ae3c32
Alex Wang <alex@awakenetworks.com>
2015-09-08 04:10:21 +0000

    main: Move the operation into its own function.
    Signed-off-by: Yeqiao Wang <ee07b291@gmail.com>

--- main.go
+++ main.go
@@ -15,6 +15,51 @@ const (
 	FLAG_OVS_COMMIT          = "ovs-commit"
 )
 
+func do_check() {
+	var duplicates []string
+	// maps between patch name to the non-duplicate 'pwclient list' line
+	patches := make(map[string]string)
+
+	cmd := exec.Command("python", "./pwclient", "list", "-s", "NEW")
+	cmd_stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalf("'pwclient list' cannot get stdout pipe: %s", err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("'pwclient list' run error: %s", err)
+	}
+	scanner := bufio.NewScanner(cmd_stdout)
+	// 'pwclient list' output has format "ID STATE    NAME"
+	re := regexp.MustCompile(`^[0-9]+  New\s+\[.*\] (.*)$`)
+	for scanner.Scan() {
+		if submatch := re.FindStringSubmatch(scanner.Text()); submatch != nil {
+			// classifies the patches
+			if _, ok := patches[re.FindStringSubmatch(scanner.Text())[1]]; ok {
+				duplicates = append(duplicates, scanner.Text())
+			} else {
+				patches[re.FindStringSubmatch(scanner.Text())[1]] = scanner.Text()
+			}
+		}
+	}
+	if scanner.Err() != nil {
+		log.Fatalf("'pwclient list' scanner error: %s", scanner.Err())
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("'pwclient list' exit error: %s", err)
+	}
+	if duplicates != nil {
+		fmt.Println("Duplicate Patches in Patchwork")
+		fmt.Println("==============================")
+		fmt.Println("ID      State        Name")
+		fmt.Println("--      -----        ----")
+		for _, dup := range duplicates {
+			fmt.Println(dup)
+		}
+	}
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "ovs-patchwork"
@@ -36,48 +81,7 @@ func main() {
 		return nil
 	}
 	app.Action = func(c *cli.Context) {
-		var duplicates []string
-		// maps between patch name to the non-duplicate 'pwclient list' line
-		patches := make(map[string]string)
-
-		cmd := exec.Command("python", "./pwclient", "list", "-s", "NEW")
-		cmd_stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatalf("'pwclient list' cannot get stdout pipe: %s", err)
-		}
-		err = cmd.Start()
-		if err != nil {
-			log.Fatalf("'pwclient list' run error: %s", err)
-		}
-		scanner := bufio.NewScanner(cmd_stdout)
-		// 'pwclient list' output has format "ID STATE    NAME"
-		re := regexp.MustCompile(`^[0-9]+  New\s+\[.*\] (.*)$`)
-		for scanner.Scan() {
-			if submatch := re.FindStringSubmatch(scanner.Text()); submatch != nil {
-				// classifies the patches
-				if _, ok := patches[re.FindStringSubmatch(scanner.Text())[1]]; ok {
-					duplicates = append(duplicates, scanner.Text())
-				} else {
-					patches[re.FindStringSubmatch(scanner.Text())[1]] = scanner.Text()
-				}
-			}
-		}
-		if scanner.Err() != nil {
-			log.Fatalf("'pwclient list' scanner error: %s", scanner.Err())
-		}
-		err = cmd.Wait()
-		if err != nil {
-			log.Fatalf("'pwclient list' exit error: %s", err)
-		}
-		if duplicates != nil {
-			fmt.Println("Duplicate Patches in Patchwork")
-			fmt.Println("==============================")
-			fmt.Println("ID      State        Name")
-			fmt.Println("--      -----        ----")
-			for _, dup := range duplicates {
-				fmt.Println(dup)
-			}
-		}
+		do_check()
 	}
 	app.Run(os.Args)
 }
