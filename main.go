acc5b513979009ea621ab5a341237d73d2aaa4e2
Alex Wang <ee07b291@gmail.com>
2016-01-18 22:27:15 +0000

    main: Add auto mark of patches.
    Signed-off-by: Yeqiao Wang <ee07b291@gmail.com>

--- main.go
+++ main.go
@@ -14,9 +14,18 @@ import (
 const (
 	FLAG_OVS_DIR             = "ovs-dir"
 	FLAG_OVS_COMMIT          = "ovs-commit"
+	FLAG_MARK_COMMITTED      = "mark-committed"
+	FLAG_MARK_DUP            = "mark-dup"
 )
 
+type Pair struct {
+	ID      string
+	line    string
+}
+
 const dateShortForm = "2006-01-02"
+const stateACCEPT   = "Accepted"
+const stateDUP      = "Not Applicable"
 
 var OVSDIR, OVSCOMMIT string
 
@@ -25,10 +34,10 @@ var OVSDIR, OVSCOMMIT string
  * entries, records the rest of entries in a string->string map and
  * returns it.
  */
-func do_duplication_check() map[string]string {
-	var duplicates, outdated []string
+func do_duplication_check() (map[string]Pair, []string) {
+	var duplicates, duplicates_ID, outdated []string
 	/* maps between patch name to the non-duplicate 'pwclient list' line. */
-	patches := make(map[string]string)
+	patches := make(map[string]Pair)
 
 	today := time.Now()
 
@@ -46,12 +55,13 @@ func do_duplication_check() map[string]string {
 	scanner := bufio.NewScanner(cmd_stdout)
 	/* 'pwclient list' output has format "ID  STATE   DATE   NAME".
 	 * extracts "DATE" and "NAME". */
-	re := regexp.MustCompile(`^[0-9]+  New    ([-0-9]+) .*   \[.*\] (.*)$`)
+	re := regexp.MustCompile(`^([0-9]+)  New    ([-0-9]+) .*   \[.*\] (.*)$`)
 	for scanner.Scan() {
 		if submatch := re.FindStringSubmatch(scanner.Text());
 		   submatch != nil {
-			dateField   := submatch[1]
-			commitField := submatch[2]
+			idField     := submatch[1]
+			dateField   := submatch[2]
+			commitField := submatch[3]
 
 			/* cherry-picks more than 30-day old patches. */
 			date, err := time.Parse(dateShortForm, dateField)
@@ -68,9 +78,11 @@ func do_duplication_check() map[string]string {
 			 * hitting a collison. */
 			if _, ok := patches[commitField]; ok {
 				duplicates = append(duplicates,
-						    patches[commitField])
+						    patches[commitField].line)
+				duplicates_ID = append(duplicates_ID,
+						       patches[commitField].ID)
 			}
-			patches[commitField] = scanner.Text()
+			patches[commitField] = Pair{idField, scanner.Text()}
 		}
 	}
 	if scanner.Err() != nil {
@@ -103,7 +115,7 @@ func do_duplication_check() map[string]string {
 		fmt.Println()
 	}
 
-	return patches
+	return patches, duplicates_ID
 }
 
 /*
@@ -111,8 +123,8 @@ func do_duplication_check() map[string]string {
  * found in "patches", it means that the patch has already been
  * upstreamed and thusly should be marked as "accepted".
  */
-func do_committed_check(patches map[string]string) {
-	var committed []string
+func do_committed_check(patches map[string]Pair) []string {
+	var committed, committed_ID []string
 
 	cmd := exec.Command("git", "log", "--oneline", "-n", "500", OVSCOMMIT)
 	cmd_stdout, err := cmd.StdoutPipe()
@@ -132,7 +144,8 @@ func do_committed_check(patches map[string]string) {
 			/* if the committed patch name is found in 'patches',
 			 * record the 'pwclient list' entry in 'committed'. */
 			if elem, ok := patches[submatch[1]]; ok {
-				committed = append(committed, elem)
+				committed = append(committed, elem.line)
+				committed_ID = append(committed_ID, elem.ID)
 			}
 		}
 	}
@@ -153,6 +166,26 @@ func do_committed_check(patches map[string]string) {
 		}
 		fmt.Println()
 	}
+
+	return committed_ID
+}
+
+/*
+ * Given array of patch 'ids' in patchwork, mark those patches
+ * to 'state'.
+ */
+func do_state_update(ids []string, state string) {
+	args := append([]string{"./pwclient", "update", "-s", state}, ids...)
+	cmd := exec.Command("python", args...)
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("'pwclient update' run error: %s", err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("'pwclient update' exit error: %s", err)
+	}
 }
 
 func main() {
@@ -169,6 +202,14 @@ func main() {
 			Name:   FLAG_OVS_COMMIT,
 			Usage:  "Commit to start check for committed patches",
 		},
+		cli.BoolFlag{
+			Name:   FLAG_MARK_COMMITTED,
+			Usage:  "Mark the committed patch as 'Accepted'",
+		},
+		cli.BoolFlag{
+			Name:   FLAG_MARK_DUP,
+			Usage:  "Mark the duplicate patch as 'Not Applicable'",
+		},
 	}
 	app.Writer = os.Stdout
 	app.Action = func(c *cli.Context) {
@@ -178,8 +219,18 @@ func main() {
 		if OVSCOMMIT = c.String(FLAG_OVS_COMMIT); OVSCOMMIT == "" {
 			log.Fatalf("must provide option --ovs-commit")
 		}
-		patches := do_duplication_check()
-		do_committed_check(patches)
+		patches, duplicates_ID := do_duplication_check()
+		committed_ID := do_committed_check(patches)
+		if c.Bool(FLAG_MARK_COMMITTED) {
+			fmt.Println("Mark Committed as Accepted")
+			fmt.Println("==========================")
+			do_state_update(committed_ID, stateACCEPT)
+		}
+		if c.Bool(FLAG_MARK_DUP) {
+			fmt.Println("Mark Dup as Not Applicable")
+			fmt.Println("==========================")
+			do_state_update(duplicates_ID, stateDUP)
+		}
 	}
 	app.Run(os.Args)
 	log.SetOutput(os.Stdout)
